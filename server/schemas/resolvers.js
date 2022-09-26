6b19db920914f844435e2fc7a0f65a1c4fa6e246
Jordan D <degco44@gmail.com>
2022-09-25 21:05:32 -0500

    typeDefs and resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,63 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ __id: context.user.__id })
+                .select('-__v -password');
+                return userData;
+            }
+            throw new AuthenticationError('User is not logged in');
+        },
+    },
+    Mutation: {
+        // Add a user
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
+        },
+        // Login authentication
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('Incorrect login');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect login');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        // Save a book to logged-in user's collection
+        saveBook: async (parent, { bookData }, context) => {
+            if (context.user) {
+                const savedBook = await User.findByIdAndUpdate(
+                    { __id: context.user.__id },
+                    { $push: { savedBooks: bookData } },
+                    { new: true, runValidators: true },
+                );
+                return savedBook;
+            }
+            throw new AuthenticationError('Must be logged in to save a book');
+        },
+        // Remove a book from logged-in user's collection
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const removeBook = await User.findByIdAndUpdate(
+                    { __id: context.user.__id },
+                    { $pull: { savedBooks: { bookId } } }
+                );
+                return removeBook;
+            }
+            throw new AuthenticationError('Must be logged in to remove a book');
+        }
+    }
+}
+
+module.exports = resolvers;
